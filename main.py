33a7d59da1dff6856448840ced6f8a91ac34e344
Eklavya Bhardwaj <57490103+eklavyabhardwaj@users.noreply.github.com>
2023-11-24 11:36:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -104,4 +104,4 @@ def download_file():
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(port = 8000, debug=True)
+    app.run(host="0.0.0.0", port=5001)
